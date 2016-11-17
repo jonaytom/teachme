e854c7e8ec0dd7b7471b4c188d0d68b0c82263c5
JTM <jonaytom@gmail.com>
2016-11-17 14:01:49 +0000

    Agregando frontend

--- app/Http/Controllers/TicketsController.php
+++ app/Http/Controllers/TicketsController.php
@@ -10,25 +10,25 @@ class TicketsController extends Controller {
 
     public function latest()
     {
-        dd('latest');
+        return view('tickets/list');
     }
 
     public function popular()
     {
-        dd('popular');
+        return view('tickets/list');
     }
     public function open()
     {
-        dd('open');
+        return view('tickets/list');
     }
 
     public function closed()
     {
-        dd('closed');
+        return view('tickets/list');
     }
 
     public function details($id)
     {
-        dd('details: ' . $id);
+        return view('tickets/details');
     }
 }
